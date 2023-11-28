9d8ad9554fb73597b51fabdf5a43239114bcc1b1
Luis Gustavo <luispedreira_9@yahoo.com.br>
2023-11-28 11:46:00 -0300

    Adicionar arquivos de projeto.

---
+++ Contact/dbo.People.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[People]
+(
+	[ContactID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NCHAR(100) NULL, 
+    [Company] NCHAR(100) NULL, 
+    [Telephone] NCHAR(100) NULL, 
+    [Email] NCHAR(100) NULL, 
+    [Client] BIT NULL, 
+    [LastCall] DATETIME NULL
+)
